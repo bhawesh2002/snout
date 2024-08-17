99ab400a62f4ad894814f2e798f1a16c9e865348
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 15:16:27 +0530

    refactor: Update main.dart to use SplashScreen instead of HomePage
    The code changes in main.dart replace the usage of the HomePage widget with the SplashScreen widget. This ensures that the splash screen is displayed as the initial screen of the app.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:snout/pages/home_page.dart';
+import 'package:snout/pages/splash_screen.dart';
 import 'package:snout/utils/measurements/uisizes.dart';
 
 void main() {
@@ -20,10 +20,10 @@ class MainApp extends StatelessWidget {
           fontFamily: 'Metropolis', scaffoldBackgroundColor: scaffoldColor),
       home: const AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle(
-          systemNavigationBarColor: scaffoldColor,
-          statusBarColor: scaffoldColor,
-        ),
-        child: HomePage(),
+            // systemNavigationBarColor: scaffoldColor,
+            // statusBarColor: scaffoldColor,
+            ),
+        child: SplashScreen(),
       ),
     );
   }
