fdefb13fe1bbec582c90af70619adc5dae54ea34
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 17:15:50 +0530

    refactor: Update home_page.dart with improved styling and layout
    The code changes in home_page.dart refactor the code to improve the styling and layout of the home page. This includes using the MetropolisText widget for displaying styled text, adding padding to the top of the container to account for the safe area, and updating the "Start Shopping" button with custom styling. These changes enhance the visual appeal and user experience of the home page.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,61 +6,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const MetropolisText(
-              'SNOUT',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 8),
-              child: Text(
-                '''Snout is a vibrant and user-friendly pet food e-commerce app designed'''
-                ''' to cater to the unique dietary needs of your furry companions. With a focus'''
-                ''' on quality and convenience, Snout offers a wide selection of premium pet'''
-                ''' foods, treats, and supplies, all curated to keep your pets healthy'''
-                ''' and happy. Whether you're looking for specialized diets or everyday essentials,'''
-                ''' Snout makes it easy to find the best products for your pets, delivered '''
-                '''straight to your door.''',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                side: const BorderSide(
-                  width: 2,
-                  color: Colors.white,
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: Placeholder(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const MetropolisText(
+                'SNOUT',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
                 ),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: MetropolisText(
-                  "Start Shopping",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    height: 1.1,
-                    fontWeight: FontWeight.w600,
+              const SizedBox(
+                height: 24,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 8),
+                child: Text(
+                  '''Snout is a vibrant and user-friendly pet food e-commerce app designed'''
+                  ''' to cater to the unique dietary needs of your furry companions. With a focus'''
+                  ''' on quality and convenience, Snout offers a wide selection of premium pet'''
+                  ''' foods, treats, and supplies, all curated to keep your pets healthy'''
+                  ''' and happy. Whether you're looking for specialized diets or everyday essentials,'''
+                  ''' Snout makes it easy to find the best products for your pets, delivered '''
+                  '''straight to your door.''',
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  side: const BorderSide(
+                    width: 2,
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: MetropolisText(
+                    "Start Shopping",
+                    style: TextStyle(
+                      fontSize: 24,
+                      height: 1.1,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
