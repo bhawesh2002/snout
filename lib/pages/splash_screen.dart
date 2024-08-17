a30aec9d979b9cf96c6921a72c334e73c7ff85a1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 15:15:38 +0530

    refactor: Update SplashScreen with animated background color and text size
    The code changes in SplashScreen.dart update the widget to include an animated background color and text size. The background color transitions between two specific colors, and the text size increases from 32 to 48 during the animation. This enhances the visual appeal of the splash screen.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,10 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:snout/pages/home_page.dart';
+import 'package:snout/utils/measurements/uisizes.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _bgColor;
+  late Animation<double> _fontSize;
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1, milliseconds: 200),
+    );
+    _bgColor = ColorTween(
+            begin: const Color.fromARGB(255, 64, 18, 93),
+            end: const Color.fromARGB(255, 141, 37, 206))
+        .animate(
+            CurvedAnimation(parent: _animationController, curve: Curves.ease));
+    _fontSize = Tween<double>(begin: 32, end: 48).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.easeInBack));
+    _animationController.repeat(reverse: true);
+    super.initState();
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Container(
+            width: UiSizes().width,
+            height: UiSizes().height,
+            decoration: BoxDecoration(color: _bgColor.value),
+            child: Center(
+              child: Text(
+                "SNOUT",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: _fontSize.value,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
