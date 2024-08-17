7736470faade99b5752c187b2b7f348cd1805b10
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 17:47:22 +0530

    refactor: Update home_page.dart with improved SnoutAppbar and styling
    The code changes in home_page.dart refactor the code to improve the styling and layout of the home page. This includes replacing the MetropolisText widget with the SnoutAppbar widget for displaying the app's top navigation bar. Additionally, the "Start Shopping" button has been updated with custom styling. These changes enhance the visual appeal and user experience of the home page.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:snout/widgets/metropolis_text.dart';
+import 'package:snout/widgets/snout_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,56 +10,21 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         child: Placeholder(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const MetropolisText(
-                'SNOUT',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 8),
-                child: Text(
-                  '''Snout is a vibrant and user-friendly pet food e-commerce app designed'''
-                  ''' to cater to the unique dietary needs of your furry companions. With a focus'''
-                  ''' on quality and convenience, Snout offers a wide selection of premium pet'''
-                  ''' foods, treats, and supplies, all curated to keep your pets healthy'''
-                  ''' and happy. Whether you're looking for specialized diets or everyday essentials,'''
-                  ''' Snout makes it easy to find the best products for your pets, delivered '''
-                  '''straight to your door.''',
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  side: const BorderSide(
-                    width: 2,
+            color: Colors.grey.shade100,
+            child: const Padding(
+              padding:
+                  EdgeInsets.only(top: 18, bottom: 12, right: 12, left: 12),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 12),
+                        child: SnoutAppbar()),
                   ),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: MetropolisText(
-                    "Start Shopping",
-                    style: TextStyle(
-                      fontSize: 24,
-                      height: 1.1,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
+            )),
       ),
     );
   }
