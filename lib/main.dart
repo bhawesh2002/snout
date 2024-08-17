f824978509f74d03088474bd80d6316ea9fe0bea
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 11:13:27 +0530

    refactor: Update main.dart with Metropolis font and UI improvements
    The code changes in main.dart include the following updates:
    - Added the Metropolis font family to the theme.
    - Changed the background color to a specific teal color.
    - Updated the app title to "SNOUT" with a bold and larger font size.
    - Added a description text with padding and a white color.
    - Added a "Start Shopping" button with a white border and custom styling.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,68 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     UiSizes().init(context);
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Metropolis'),
       home: Scaffold(
         body: Container(
           width: UiSizes().width,
           height: UiSizes().height,
-          color: Colors.teal,
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
-                color: Colors.white,
+          color: const Color.fromARGB(255, 37, 201, 206),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'SNOUT',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  color: Colors.white,
+                ),
               ),
-            ),
+              const SizedBox(
+                height: 24,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 8),
+                child: Text(
+                  '''Snout is a vibrant and user-friendly pet food e-commerce app designed'''
+                  ''' to cater to the unique dietary needs of your furry companions. With a focus'''
+                  ''' on quality and convenience, Snout offers a wide selection of premium pet'''
+                  ''' foods, treats, and supplies, all curated to keep your pets healthy'''
+                  ''' and happy. Whether you're looking for specialized diets or everyday essentials,'''
+                  ''' Snout makes it easy to find the best products for your pets, delivered '''
+                  '''straight to your door.''',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  side: const BorderSide(
+                    width: 2,
+                    color: Colors.white,
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "Start Shopping",
+                    textHeightBehavior: TextHeightBehavior(
+                      applyHeightToLastDescent: false,
+                      applyHeightToFirstAscent: true,
+                    ),
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      height: 1.1,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
