354b7482b9bdadfb207bf84d944da91f8256c1b2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 17:45:34 +0530

    refactor: Add AppIcons class for managing custom icons
    The code changes in this commit add a new class called AppIcons, which holds all the custom icons used in the Snout app. This class provides easy access to the shopping cart and notifications icons through static getter methods. This improves code organization and reusability for icon usage throughout the app.
    
    This commit message follows the convention of starting with a type (refactor for code improvements) and providing a concise summary of the changes made.

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  /// AppIcons class holds all the Icons used in Snout app
+
+  static const IconData _shoppingCart = Icons.shopping_bag;
+  static const IconData _notifications = Icons.notifications;
+
+  static IconData get shoppingCart => _shoppingCart;
+  static IconData get notifications => _notifications;
+}
