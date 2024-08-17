4a525cc8b7b3dfa894ead7297e9d19776f6b19a4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 19:11:53 +0530

    refactor: Update SnoutIconButton widget with customizable size and styling
    This commit updates the SnoutIconButton widget in snout_icon_button.dart to include additional properties for customizing the size, border width, border radius, border color, background color, icon color, splash color, and shape of the button. These enhancements provide more flexibility and control over the appearance of circular icon buttons throughout the app.

--- lib/widgets/snout_icon_button.dart
+++ lib/widgets/snout_icon_button.dart
@@ -3,18 +3,51 @@ import 'package:flutter/material.dart';
 class SnoutIconButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback? onTap;
-  const SnoutIconButton({super.key, required this.icon, this.onTap});
+  final double? size;
+  final double? borderWidth;
+  final double? borderRadius;
+  final Color? borderColor;
+  final Color? bgColor;
+  final Color? iconColor;
+  final Color? splashColor;
+  final BoxShape? shape;
+  const SnoutIconButton(
+      {super.key,
+      required this.icon,
+      this.onTap,
+      this.size,
+      this.borderWidth,
+      this.borderColor,
+      this.bgColor,
+      this.iconColor,
+      this.shape = BoxShape.circle,
+      this.borderRadius,
+      this.splashColor});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      borderRadius: BorderRadius.circular(24),
+      customBorder: shape == BoxShape.circle ? const CircleBorder() : null,
+      borderRadius: BorderRadius.circular(borderRadius ?? 0),
       onTap: onTap ?? () {},
-      child: CircleAvatar(
-        radius: 24,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.black,
-        child: Icon(icon),
+      splashColor: splashColor,
+      child: Container(
+        padding: EdgeInsets.all(size ?? 12),
+        decoration: BoxDecoration(
+          color: bgColor ?? Colors.transparent,
+          shape: shape!,
+          border: Border.all(
+            width: borderWidth ?? 0,
+            color: borderColor ?? Colors.transparent,
+          ),
+          borderRadius: shape == BoxShape.rectangle
+              ? BorderRadius.circular(borderRadius ?? 0)
+              : null,
+        ),
+        child: Icon(
+          icon,
+          color: iconColor ?? Colors.black,
+        ),
       ),
     );
   }
