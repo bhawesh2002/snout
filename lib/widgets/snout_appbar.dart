903482683fd7a38425238280c553346fe3e1f1d5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-17 17:46:23 +0530

    feat: Add SnoutAppbar widget for displaying the app's top navigation bar
    This commit adds a new widget called SnoutAppbar, which represents the top navigation bar of the Snout app. It includes the app name, notification icon, and shopping cart icon. This widget improves code organization and reusability for displaying the app's top navigation bar.
    
    This commit message follows the convention of starting with a type (feat for a new feature) and providing a concise summary of the changes made.

---
+++ lib/widgets/snout_appbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:snout/utils/app_icons.dart';
+import 'package:snout/widgets/metropolis_text.dart';
+import 'package:snout/widgets/snout_icon_button.dart';
+
+class SnoutAppbar extends StatelessWidget {
+  const SnoutAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const MetropolisText(
+          "SNOUT",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Expanded(child: SizedBox()),
+        SnoutIconButton(icon: AppIcons.notifications),
+        const SizedBox(width: 4),
+        SnoutIconButton(icon: AppIcons.shoppingCart)
+      ],
+    );
+  }
+}
