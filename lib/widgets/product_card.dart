664dda90ca1ffe6a2f38573d8b1b51c2594a3fcb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-19 20:09:03 +0530

    refactor: Update ProductCard widget with discount label and pet icon
    This commit updates the ProductCard widget in product_card.dart to include a discount label and a pet icon. The discount label is displayed as a rotated text in the top right corner of the card, indicating a 10% discount. The pet icon is positioned in the top left corner of the card, representing the type of animal the food is for. These enhancements improve the visual appeal and user experience of the Snout app's product cards.

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:snout/utils/app_icons.dart';
+import 'dart:math' as math;
+
+import 'package:snout/widgets/metropolis_text.dart';
 
 class ProductCard extends StatelessWidget {
   final String? productName;
@@ -13,86 +16,137 @@ class ProductCard extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(18),
           border: Border.all(width: 1, color: const Color(0xffEFEFEF))),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 150,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'lib/assets/images/product_package_1.png',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Kennel Kitchen Wet Dog Food - Lamb Chunks in Gravy (Pack of 15 x 80g Pouches)',
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(AppIcons.star, size: 18),
-                const SizedBox(width: 4),
-                const Text(
-                  '4.2',
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  '₹ 2,456',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800,
-                    letterSpacing: 0.5,
-                  ),
-                ),
-                SizedBox(width: 6),
-                Text(
-                  '₹3,124',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      decoration: TextDecoration.lineThrough,
-                      height: 1.5),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                  color: const Color(0xffE5E5E5),
-                  borderRadius: BorderRadius.circular(6)),
-              child: const Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    "ADD TO CART",
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w800,
+      child: Stack(
+        children: [
+          //Label Displaying Discount Percentage
+          Positioned.fill(
+            child: Transform.rotate(
+              angle: math.pi / 4,
+              origin: const Offset(
+                  30, -100), //Calculated Value for lable Width of 120
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 15,
+                  width: 120, //set the width to 120
+                  color: Colors.grey.shade800,
+                  child: const Center(
+                    child: MetropolisText(
+                      "10% Off",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 8,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          //Icon for displaying for which animal food is for
+          Positioned.fill(
+            top: 5,
+            left: 5,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey.shade200.withOpacity(0.4)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Icon(
+                    AppIcons.pet,
+                    size: 14,
+                    color: Colors.black.withOpacity(0.7),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'lib/assets/images/product_package_1.png',
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Kennel Kitchen Wet Dog Food - Lamb Chunks in Gravy (Pack of 15 x 80g Pouches)',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(AppIcons.star, size: 18),
+                    const SizedBox(width: 4),
+                    const Text(
+                      '4.2',
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '₹ 2,456',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                    SizedBox(width: 6),
+                    Text(
+                      '₹3,124',
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          decoration: TextDecoration.lineThrough,
+                          height: 1.5),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                      color: const Color(0xffE5E5E5),
+                      borderRadius: BorderRadius.circular(6)),
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12.0),
+                      child: Text(
+                        "ADD TO CART",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
