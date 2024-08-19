acdb18d16a11f76de462dda248746367038fbeb5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-19 08:02:46 +0530

    feat: Add lazy loading for images and grid view for popular products
    This commit adds lazy loading for images and a grid view for displaying popular products in the Snout app's home page. The lazy loading feature improves page load performance by loading images only when they are visible on the screen. The grid view provides a visually appealing layout for showcasing multiple products. These enhancements enhance the user experience and make the app more engaging.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:snout/widgets/metropolis_text.dart';
 import 'package:snout/widgets/pet_selection_mini.dart';
+import 'package:snout/widgets/product_card.dart';
 import 'package:snout/widgets/snout_appbar.dart';
 import 'package:snout/widgets/snout_text_field.dart';
 
@@ -17,13 +20,49 @@ class HomePage extends StatelessWidget {
             bottom: 10),
         child: Placeholder(
           color: Colors.grey.shade100,
-          child: const Column(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              SnoutAppbar(),
-              SizedBox(height: 12),
-              SnoutTextField(),
-              SizedBox(height: 32),
-              PetSelectionMini()
+              const SnoutAppbar(),
+              const SizedBox(height: 12),
+              const SnoutTextField(),
+              const SizedBox(height: 32),
+              const PetSelectionMini(),
+              const SizedBox(height: 32),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    children: [
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: MetropolisText(
+                          "Popular Products",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      Expanded(
+                        child: MasonryGridView.builder(
+                          shrinkWrap: true,
+                          itemCount: 12,
+                          mainAxisSpacing: 18,
+                          crossAxisSpacing: 16,
+                          gridDelegate:
+                              const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          itemBuilder: (context, index) {
+                            return const ProductCard();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
